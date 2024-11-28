ba444cd38f02e9b54501a45a77b0aef52938e8f3
mouadboufenzi <mouadboufenzi2001@gmail.com>
2024-11-28 12:57:59 +0100

    Initial commit

---
+++ src/main/java/com/myapp/springjpaworkshop/SpringJpAworkshopApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.springjpaworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpAworkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpAworkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myapp/springjpaworkshop/SpringJpAworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springjpaworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpAworkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
